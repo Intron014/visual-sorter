f2f7d9dc2e828eafc911aafb4b75fe3b5a7d6791
Jorge Benjumea Burillo <me@intron014.com>
2023-06-08 09:05:15 +0200

    The almighty sorter is here

--- plot.py
+++ plot.py
@@ -35,41 +35,28 @@ ax.set_title("Bubble Sort Visualization")
 
 # Variables for user input
 num_items = int(input("Enter the number of items to sort: "))
-num_iterations = int(input("Enter the number of iterations: "))
 speed = int(input("Enter the speed of the animation (smaller number means faster speed): "))
 
-total_swaps = 0
-total_comparisons = 0
+bar_rects = ax.bar(range(len(generate_list(num_items))), generate_list(num_items), align="edge")
 
-for _ in range(num_iterations):
-    bar_rects = ax.bar(range(len(generate_list(num_items))), generate_list(num_items), align="edge")
+# Function to be called repeatedly to update the animation
+def animate(frame_number):
+    global bar_rects
+    try:
+        generator = bubble_sort(bar_rects)
+        rects, swaps, comparisons = next(generator)
+        if debug_mode:
+            print(f"Swaps: {swaps}\tComparisons: {comparisons}")
+    except StopIteration:
+        if debug_mode:
+            print("Sorting completed")
+        # Regenerate a new list and reset the bar graph
+        new_list = generate_list(num_items)
+        for rect, height in zip(bar_rects, new_list):
+            rect.set_height(height)
 
-    # Function to be called repeatedly to update the animation
-    def animate(frame_number):
-        global bar_rects, total_swaps, total_comparisons
-        try:
-            generator = bubble_sort(bar_rects)
-            rects, swaps, comparisons = next(generator)
-            total_swaps += swaps
-            total_comparisons += comparisons
-            if debug_mode:
-                print(f"Swaps: {swaps}\tComparisons: {comparisons}")
-        except StopIteration:
-            if debug_mode:
-                print("Sorting completed")
-            # Regenerate a new list and reset the bar graph
-            new_list = generate_list(num_items)
-            for rect, height in zip(bar_rects, new_list):
-                rect.set_height(height)
+# Create the animation
+anim = animation.FuncAnimation(fig, animate, interval=speed, repeat=True)
 
-    # Create the animation
-    anim = animation.FuncAnimation(fig, animate, interval=speed, repeat=False)
-
-    # Show the plot
-    plt.show()
-
-# Calculate average swaps and comparisons
-avg_swaps = total_swaps / num_iterations
-avg_comparisons = total_comparisons / num_iterations
-print(f"Average Swaps: {avg_swaps}")
-print(f"Average Comparisons: {avg_comparisons}")
+# Show the plot
+plt.show()
